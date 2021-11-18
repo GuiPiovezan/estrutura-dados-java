3e3dfb135bb779c93f798f8a17518b0b6e3f373d
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-18 09:55:26 -0300

    Criando o método busca de um elemento de uma determinada posição do vetor

---
+++ src/packages/Vetor/Teste/Aula05.java
@@ -0,0 +1,19 @@
+package packages.Vetor.Teste;
+
+import packages.Vetor.Vetor;
+
+public class Aula05 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("Sara");
+        vetor.adiciona("Guilherme");
+        vetor.adiciona("Pedro");
+        vetor.adiciona("Matheus");
+        vetor.adiciona("Reinaldo");
+
+        System.out.println(vetor.getTamanho());
+        System.out.println(vetor);
+        System.out.println(vetor.busca(4));
+    }
+}
--- src/packages/Vetor/Vetor.java
+++ src/packages/Vetor/Vetor.java
@@ -40,6 +40,14 @@ public class Vetor{
         return false;
     }
 
+    public String busca(int posicao){
+        
+        if(!(posicao >=0 && posicao < this.tamanho)){
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        return this.elementos[posicao];
+    }
+
     public int getTamanho(){
         return this.tamanho;
     }
