ee89b498cb86724478672a20d4117c2cf8f1115e
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-23 10:59:12 -0300

    Conversões de decimais para demais tipos de bases, como octal e hexadecimal

--- src/packages/pilhas/desafios/Desafio03.java
+++ src/packages/pilhas/desafios/Desafio03.java
@@ -1,5 +1,7 @@
 package packages.pilhas.desafios;
 
+import java.util.Stack;
+
 import packages.pilhas.Pilha;
 
 public class Desafio03 {
@@ -7,7 +9,17 @@ public class Desafio03 {
         System.out.println("25 decimal em binário " + conversaoBinario(25));
         System.out.println("144 decimal em binário " + conversaoBinario(144));
         System.out.println("256 decimal em binário " + conversaoBinario(256));
+        imprimeResultadoQualquerBase(25, 2);
+        imprimeResultadoQualquerBase(25, 8);
+        imprimeResultadoQualquerBase(25, 16);
+        imprimeResultadoQualquerBase(536, 16);
+        imprimeResultadoQualquerBase(1025699, 16);
     }
+
+    public static void imprimeResultadoQualquerBase(int numero, int base){
+		System.out.println(numero + " na base " + base + 
+				" é: " + decimalQualquerBase(numero, base));
+	}
 
     public static String conversaoBinario(int valor){
 
@@ -25,4 +37,28 @@ public class Desafio03 {
 
         return stringBuilder.toString();
     }
+
+    public static String decimalQualquerBase(int numero, int base){
+		
+		if (base > 16){
+			throw new IllegalArgumentException();
+		}
+
+		Stack<Integer> pilha = new Stack<>();
+		String numBase = "";
+		int resto;
+		String bases = "0123456789ABCDEF";
+
+		while (numero > 0){
+			resto = numero % base;
+			pilha.push(resto);
+			numero /= base;
+		}
+
+		while (!pilha.isEmpty()){
+			numBase += bases.charAt(pilha.pop());
+		}
+
+		return numBase;
+	}
 }
